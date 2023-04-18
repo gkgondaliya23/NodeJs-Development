eacd744bea32e0225173025e760310c629e12d6e
Girish Gondaliya <gkgondaliya23@gmail.com>
2023-04-18 08:28:03 +0530

    MongoDB

---
+++ MongoDB/s.js
@@ -0,0 +1,6 @@
+db.students.insertOne({ name: "Raj", rollno: 101, marks: 98 });
+db.students.insertOne({ name: "Ravi", rollno: 102, marks: 99 });
+db.students.insertOne({ name: "Shiva", rollno: 103, marks: 100 });
+db.students.insertOne({ name: "Pavan", rollno: 104, marks: 80 });
+// load("D:\student.js");
+// db.students.find();
